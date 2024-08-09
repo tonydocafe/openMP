2b364c0c5615e2badbc423a9787acd63cbcb30bf
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2024-08-09 16:56:43 -0300

    Update soma_acumulada.c

--- soma_acumulada.c
+++ soma_acumulada.c
@@ -4,28 +4,28 @@
 #include <time.h>
 #define N 8
 
-int main(){
+int main() {
     int b[N];
     int s = 0;
     
     srand(time(NULL));
-    for(int i = 0; i < N; i++)
-        b[i] = rand()%10;
+    for (int i = 0; i < N; i++)
+        b[i] = rand() % 10;
     
     printf("\n");
-    for(int i = 0; i < N; i++)
-        printf("%d ",b[i]);
+    for (int i = 0; i < N; i++)
+        printf("%d ", b[i]);
     printf("\n");
     
     #pragma omp parallel
     {
-        #pragma omp for
-        for(int i = 0; i < N; i++)
-            s = s + b[i];
+        #pragma omp for reduction(+:s)
+        for (int i = 0; i < N; i++)
+            s += b[i];
     }
+    
     printf("\n\n");
-
-    printf("s = %d\n",s);
+    printf("s = %d\n", s);
     
     return 0;
 }
